06af95ec680a1926dc9d7fd21c70c5b3281b802b
S. M. Mehedi <s.mehedi2022@gmail.com>
2024-10-04 20:01:19 +0600

    contact done

--- src/Components/ContactComponent.tsx
+++ src/Components/ContactComponent.tsx
@@ -1,7 +1,32 @@
 import { Facebook, Github, Linkedin, Mail, MapPin, Phone } from "lucide-react";
-import React from "react";
+import React, { FormEvent } from "react";
 
 const ContactComponent: React.FC = () => {
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+
+    formData.append("access_key", "6117916a-f9b4-41b3-b553-4ee20ab1e893");
+
+    console.log(formData);
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      alert(
+        "Suucessfully notified to Mehedi. You will get in touch from Mehedi soon. Thank you."
+      );
+      event.currentTarget.reset();
+    } else {
+      alert(
+        "Oops, Somthing went wrong. Try again or contect Mehedi by given information. Thank you."
+      );
+    }
+  };
   return (
     <>
       <section id="contact" className="py-20 bg-gray-200">
@@ -28,19 +53,22 @@ const ContactComponent: React.FC = () => {
                 <h4 className="text-lg font-semibold mb-4">Follow Me</h4>
                 <div className="flex space-x-4">
                   <a
-                    href="#"
+                    href="https://github.com/mehedi1dev"
+                    target="_blank"
                     className="text-gray-600 hover:text-gray-900 transition-colors"
                   >
                     <Github className="w-6 h-6" />
                   </a>
                   <a
-                    href="#"
+                    href="https://www.linkedin.com/in/mehedi1dev/"
+                    target="_blank"
                     className="text-gray-600 hover:text-gray-900 transition-colors"
                   >
                     <Linkedin className="w-6 h-6" />
                   </a>
                   <a
-                    href="#"
+                    href="https://www.facebook.com/mehedi1dev/"
+                    target="_blank"
                     className="text-gray-600 hover:text-gray-900 transition-colors"
                   >
                     <Facebook className="w-6 h-6" />
@@ -49,22 +77,37 @@ const ContactComponent: React.FC = () => {
               </div>
             </div>
             <div className="md:w-1/2 p-8 bg-gray-100">
-              <form className="space-y-4">
+              <form className="space-y-4" onSubmit={onSubmit}>
                 <input
                   type="text"
+                  name="name"
                   placeholder="Your Name"
                   className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-gray-500"
+                  required
                 />
                 <input
                   type="email"
+                  name="email"
                   placeholder="Your Email"
+                  className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-gray-500"
+                  required
+                />
+                <input
+                  type="text"
+                  name="mobile"
+                  placeholder="Phone No. (Optional)"
                   className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-gray-500"
                 />
                 <textarea
                   placeholder="Your Message"
+                  name="Message"
                   className="w-full p-2 border border-gray-300 rounded h-32 focus:outline-none focus: focus:ring-2 focus:ring-gray-500"
+                  required
                 ></textarea>
-                <button className="w-full bg-gray-700 text-white py-2 px-4 rounded hover:bg-gray-800 transition-colors">
+                <button
+                  className="w-full bg-gray-700 text-white py-2 px-4 rounded hover:bg-gray-800 transition-colors"
+                  type="submit"
+                >
                   Send Message
                 </button>
               </form>
