96a146a9c3dd978ef870e5676f3e6e129700290d
S. M. Mehedi <s.mehedi2022@gmail.com>
2024-10-04 19:04:32 +0600

    exp done

--- src/Components/ExperienceComponent.tsx
+++ src/Components/ExperienceComponent.tsx
@@ -2,15 +2,36 @@ import React from "react";
 
 const experience = [
   {
-    title: "Full-Stack Developer",
-    company: "WebSolutions Co.",
-    location: "San Francisco, CA",
+    title: "Full-Stack Web Developer",
+    company: "Asiatic Mindshare Ltd",
+    location: "Banani, Dhaka, Bangladesh",
     period: "May 2023 - Present",
     responsibilities: [
-      "Developed and maintained 'ClientConnect', a suite of over 10 client websites using React, Node.js, and MongoDB, ensuring 99.9% uptime and optimal performance.",
-      "Implemented 'ResponsivePro', our responsive design framework, and 'ProgressivePWA', our progressive web app feature set, improving mobile user experience and increasing mobile traffic by 35%.",
-      "Integrated 'PayEase', our custom payment gateway, streamlining business processes for clients and reducing manual data entry by 70%.",
-      "Collaborated with UX designers to implement 'IntuitiveUI', our intuitive user interface framework, resulting in a 20% increase in user satisfaction scores.",
+      {
+        title: "Task Management Software",
+        description:
+          "Developed front-end with React, TypeScript, React Query,  Axios, Ant Design (AntD), and Tailwind CSS, ensuring smooth task management.",
+      },
+      {
+        title: "Media Management Software",
+        description:
+          "Contributed to front-end using React, React Query,  Axios, Ant Design (AntD), and Tailwind CSS, streamlining media workflows.",
+      },
+      {
+        title: "Requisition & Expense Management (Back-end)",
+        description:
+          "Built backend with Node.js, Express.js, Mongoose, and MongoDB for efficient requisition and expense handling.",
+      },
+      {
+        title: "Requisition & Expense Management (Front-end)",
+        description:
+          "Developed front-end with React, Material UI, Axios, and Tailwind CSS, enhancing user interface for requisition & expense management.",
+      },
+      {
+        title: "IT Service Management Application",
+        description:
+          "Created front-end components using React, Redux, and Ant Design (AntD), optimizing IT service automation.",
+      },
     ],
   },
 ];
@@ -26,16 +47,23 @@ const ExperienceComponent: React.FC = () => {
           <div className="max-w-3xl mx-auto space-y-8">
             {experience.map((job, index) => (
               <div key={index} className="bg-white p-6 rounded-lg shadow-lg">
-                <h3 className="text-xl font-bold mb-2">{job.title}</h3>
+                <h3 className="text-xl font-bold mb-2 uppercase">
+                  {job.title}
+                </h3>
                 <p className="text-gray-600 mb-2">
                   <strong>{job.company}</strong> | {job.location} | {job.period}
                 </p>
                 <p className="text-gray-600 mb-4">{job.period}</p>
-                <ul className="list-disc list-inside space-y-2">
-                  {job.responsibilities.map((resp, idx) => (
-                    <li key={idx}>{resp}</li>
-                  ))}
-                </ul>
+
+                {job.responsibilities.map((resp, idx) => (
+                  <div className="flex gap-x-2 mt-2">
+                    <p className="font-bold">#</p>
+                    <p key={idx}>
+                      <span className="font-bold">{resp.title}</span> :{" "}
+                      <span className="">{resp.description}</span>
+                    </p>
+                  </div>
+                ))}
               </div>
             ))}
           </div>
